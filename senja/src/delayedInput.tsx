8097fd55d4ce0e6beb61d07725d7c534422ece93
alunity <75143943+alunity@users.noreply.github.com>
2022-11-28 21:00:24 +0000

    Improved caching of stock prices

--- senja/src/delayedInput.tsx
+++ senja/src/delayedInput.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { isSymbol } from "./search";
 
 interface input {
   callback: Function;
@@ -9,32 +8,12 @@ function DelayedInput(props: input) {
   let [text, setText] = useState("");
 
   useEffect(() => {
-    if (isSymbol(text)) {
-      props.callback(text);
-    }
+    props.callback(text);
   }, [text, props]);
 
-  // function getSymboL()
-
-  // let options;
-  // let suggestions = search(text);
-  // if (suggestions.length < 50) {
-  //   options = suggestions.map((x) => {
-  //     return (
-  //       <option key={x[0]} value={x[0]}>
-  //         {x[1]}
-  //       </option>
-  //     );
-  //   });
-  // }
   return (
     <div>
-      <input
-        value={text}
-        // list="suggestions"
-        onChange={(e) => setText(e.target.value)}
-      ></input>
-      {/* <datalist id="suggestions">{options}</datalist> */}
+      <input value={text} onChange={(e) => setText(e.target.value)}></input>
     </div>
   );
 }
--- senja/src/stock.ts
+++ senja/src/stock.ts
@@ -1,31 +1,66 @@
-interface cacheI {
+interface isymbols {
   [symbol: string]: number;
 }
 
-let cache: cacheI = {};
+class symbolDB {
+  symbols: isymbols = {};
+  nullSymbols: Array<string> = [];
 
-async function requestPrice(symbol: string): Promise<number> {
-  let response = fetch(
-    `https://api.allorigins.win/get?url=${encodeURIComponent(
-      "https://query1.finance.yahoo.com/v8/finance/chart/" +
-        symbol.toUpperCase()
-    )}`,
-    { cache: "no-store" }
-    // Why storing??
-  );
-  return JSON.parse((await (await response).json()).contents).chart.result[0]
-    .meta.regularMarketPrice;
+  addSymbol(symbol: string, price: number) {
+    this.symbols[symbol] = price;
+  }
+
+  addNullSymbol(symbol: string) {
+    this.nullSymbols.push(symbol);
+  }
+
+  checkNullSymbolExists(symbol: string): boolean {
+    return this.nullSymbols.indexOf(symbol) > -1;
+  }
+
+  checkSymbolExists(symbol: string): boolean {
+    return symbol in this.symbols;
+  }
+
+  getPrice(symbol: string): number {
+    return this.symbols[symbol];
+  }
+}
+
+const DB = new symbolDB();
+
+async function requestData(symbol: string): Promise<number> {
+  try {
+    let response = fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        "https://query1.finance.yahoo.com/v8/finance/chart/" +
+          symbol.toUpperCase()
+      )}`,
+      { cache: "no-store" }
+      // Why storing??
+    );
+    let data = JSON.parse((await (await response).json()).contents);
+    return data.chart.result[0].meta.regularMarketPrice;
+  } catch {
+    return -1;
+  }
 }
 
 async function getPrice(symbol: string): Promise<number> {
-  symbol = symbol.toLowerCase();
-  if (cache[symbol] !== undefined) {
-    return cache[symbol];
-  } else {
-    let price = await requestPrice(symbol);
-    cache[symbol] = price;
-    return price;
+  symbol = symbol.toUpperCase();
+  if (DB.checkNullSymbolExists(symbol)) {
+    return -1;
+  } else if (DB.checkSymbolExists(symbol)) {
+    return DB.getPrice(symbol);
   }
+  let price = await requestData(symbol);
+
+  if (price === -1) {
+    DB.addNullSymbol(symbol);
+    return -1;
+  }
+  DB.addSymbol(symbol, price);
+  return price;
 }
 
 export default getPrice;
