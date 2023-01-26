913bfae080329352108e01e532bc71e762858072
Alexander Litchev <75143943+alunity@users.noreply.github.com>
2023-01-26 20:22:28 +0000

    Reset project

---
+++ stonks/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+function App() {
+  return <div className="App">hello world</div>;
+}
+
+export default App;
