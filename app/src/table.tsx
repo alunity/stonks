01a9c7d6721e811a1f90ec0439eda7cdacb17ab8
Alexander Litchev <75143943+alunity@users.noreply.github.com>
2023-02-03 23:13:27 +0000

    Added price change to table

--- app/src/table.tsx
+++ app/src/table.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import iPortfolio from "./iPortfolio";
 import { getSymbolData } from "./yfinance";
+import { Tooltip } from "bootstrap";
 
 interface iTable {
   portfolio: iPortfolio;
@@ -11,12 +12,14 @@ interface row {
   symbol: string;
   amount: number;
   price: number;
+  prevClosePrice: number;
 }
 
 interface iTotal {
   price: number;
   amount: number;
   totalPrice: number;
+  totalPrevPrice: number;
 }
 
 function Table(props: iTable) {
@@ -32,6 +35,7 @@ function Table(props: iTable) {
             symbol: symbolData.chart.result[0].meta.symbol,
             amount: props.portfolio[i],
             price: symbolData.chart.result[0].meta.regularMarketPrice,
+            prevClosePrice: symbolData.chart.result[0].meta.chartPreviousClose,
           });
         }
       }
@@ -41,14 +45,59 @@ function Table(props: iTable) {
     getData();
   }, [props.portfolio]);
 
-  let total: iTotal = { price: 0, amount: 0, totalPrice: 0 };
+  useEffect(() => {
+    // Initialize tooltips
+    const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+      (tooltipTriggerEl) => new Tooltip(tooltipTriggerEl)
+    );
+  });
+
+  let total: iTotal = { price: 0, amount: 0, totalPrice: 0, totalPrevPrice: 0 };
   let rows = data?.map((x: row) => {
     total.price += x.price;
     total.amount += x.amount;
     total.totalPrice += x.price * x.amount;
+    total.totalPrevPrice += x.prevClosePrice * x.amount;
     return (
       <tr key={x.symbol} onClick={() => props.setSymbol(x.symbol)}>
-        <td>{x.symbol}</td>
+        <td>
+          <span
+            className={
+              "fw-bold " +
+              (x.price > x.prevClosePrice
+                ? "text-success-emphasis"
+                : x.price === x.prevClosePrice
+                ? ""
+                : "text-danger-emphasis")
+            }
+          >
+            {x.price > x.prevClosePrice
+              ? "↑ "
+              : x.price === x.prevClosePrice
+              ? ""
+              : "↓ "}
+          </span>
+          {x.symbol}
+        </td>
+        <td
+          className={
+            "fw-bold " +
+            (x.price > x.prevClosePrice
+              ? "text-success-emphasis"
+              : x.price === x.prevClosePrice
+              ? ""
+              : "text-danger-emphasis")
+          }
+        >
+          {(x.price > x.prevClosePrice ? "+" : "") +
+            (((x.price - x.prevClosePrice) / x.prevClosePrice) * 100).toFixed(
+              2
+            ) +
+            "%"}
+        </td>
         <td>{x.amount}</td>
         <td>{x.price.toFixed(2)}</td>
         <td>{(x.price * x.amount).toFixed(2)}</td>
@@ -59,8 +108,45 @@ function Table(props: iTable) {
     rows.push(
       <tr key={"total"}>
         <td>
-          <strong>Total</strong>
+          <strong>
+            <span
+              className={
+                "fw-bold " +
+                (total.totalPrice > total.totalPrevPrice
+                  ? "text-success-emphasis"
+                  : total.totalPrice === total.totalPrevPrice
+                  ? ""
+                  : "text-danger-emphasis")
+              }
+            >
+              {total.totalPrice > total.totalPrevPrice
+                ? "↑ "
+                : total.totalPrice === total.totalPrevPrice
+                ? ""
+                : "↓ "}
+            </span>
+            Total
+          </strong>
         </td>
+        <td
+          className={
+            "fw-bold " +
+            (total.totalPrice > total.totalPrevPrice
+              ? "text-success-emphasis"
+              : total.totalPrice === total.totalPrevPrice
+              ? ""
+              : "text-danger-emphasis")
+          }
+        >
+          {(total.totalPrice > total.totalPrevPrice ? "+" : "") +
+            (
+              ((total.totalPrice - total.totalPrevPrice) /
+                total.totalPrevPrice) *
+              100
+            ).toFixed(2) +
+            "%"}
+        </td>
+
         <td>
           <strong>{total.amount}</strong>
         </td>
@@ -83,6 +169,15 @@ function Table(props: iTable) {
             <thead>
               <tr>
                 <th scope="col">symbol</th>
+                <th scope="col">
+                  <span
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    data-bs-title="Change from previous market close"
+                  >
+                    change
+                  </span>
+                </th>
                 <th scope="col">amount</th>
                 <th scope="col">price</th>
                 <th scope="col">total</th>
