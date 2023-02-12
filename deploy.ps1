a0d7d95839941792100dc97523fd845b8adbdfd0
Alexander Litchev <75143943+alunity@users.noreply.github.com>
2023-02-12 16:10:37 +0000

    Added README.MD

---
+++ deploy.ps1
@@ -0,0 +1,27 @@
+cd app
+
+# abort on errors
+# set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+# echo > .nojekyll
+ni .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/alunity/stonks.git main:gh-pages
+
+cd -
